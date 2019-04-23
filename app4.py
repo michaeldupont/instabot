2225d733cafe8821adfccdf647b491144a8c3a85
MichaelDupont <michael@homedupont.fr>
2019-04-23 11:51:03 +0200

    gestion du json

--- app4.py
+++ app4.py
@@ -60,6 +60,7 @@ def MediaData():
         with open("data3.json", "w") as f:
             f.write(json.dumps(data3, indent=4)) 
         
+        exemple = data3["users"][0]["pk"]
         with open("log.txt", "w") as f:
             f.write(user)
             f.write(pwd)
@@ -67,7 +68,7 @@ def MediaData():
             f.write(pwddb)
             f.write(db)
             f.write(mediaID)
-            f.write(data3["users"][0]["pk"])
+            f.write(exemple)
             f.close()
                 
         for i in range(10):
