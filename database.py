366249a2dfe1df3f412da145ceb0d1f7560c8e0a
MichaelDupont <michael@homedupont.fr>
2019-05-07 17:40:49 +0200

    manque qqs fichiers

--- database.py
+++ database.py
@@ -11,21 +11,70 @@ def inputdb(media_likers):
     pwd = conf["DB"]["PASSWORD"]
     db = conf["DB"]["DATABASE"]
 
-
     mariadb_connection = mariadb.connect(user= username, password=pwd, database=db)
     cursor = mariadb_connection.cursor()
-    cursor.execute("INSERT INTO `medias_likers`(`number`, \
-        `date1`, `date2`, `civ`, `nom1`, `prenom`, `L2`, `L3`, `L4`, `L5`, `CP`, `loc`) \
-        VALUES (NULL,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)", (words[0], words[1],words[2],words[3],words[4],words[5],words[6],words[7],words[8],words[9],words[10]))
+    
+    cursor.execute( "INSERT INTO projet (nom, date_creation, cloture, responsable_id) \
+                    SELECT \
+                        projet.nom || ' Perso ' || utilisateur.prenom, CURRENT_TIMESTAMP, FALSE, projet.responsable_id \
+                    FROM \
+                    projet \
+                    JOIN utilisateur ON utilisateur.id = projet.responsable_id \
+                    WHERE projet.id IN (18, 42); \
+                               ")
+
     mariadb_connection.commit()
     mariadb_connection.close()
 
-############ ouverture de fichier et lecture
 
-with open('data_test.txt', 'r') as mon_fichier:
-    print("nom du fichier", mon_fichier.name)
-    while 1:
-        ligne=mon_fichier.readline()
-        if ligne =="":
-            break
-        inputdb(ligne)
+def insertmedia(MediaID, MediaURL, nbr_likers):
+    with open("config.json","r") as fichier:
+        conf = json.load(fichier) 
+    
+    username = conf["DB"]["USER"]
+    pwd = conf["DB"]["PASSWORD"]
+    db = conf["DB"]["DATABASE"]
+
+    mariadb_connection = mariadb.connect(user= username, password=pwd, database=db)
+    cursor = mariadb_connection.cursor()
+    
+    cursor.execute( "INSERT INTO Media (MediaID, MediaURL, nbr_likers) \
+                    VALUES (MediaID, MediaURL,nbr_likers) \
+                               ")
+    mariadb_connection.commit()
+    mariadb_connection.close()
+
+def insertpeople(pk,username,full_name,compjson):
+    with open("config.json","r") as fichier:
+        conf = json.load(fichier) 
+    
+    username = conf["DB"]["USER"]
+    pwd = conf["DB"]["PASSWORD"]
+    db = conf["DB"]["DATABASE"]
+
+    mariadb_connection = mariadb.connect(user= username, password=pwd, database=db)
+    cursor = mariadb_connection.cursor()
+    
+    cursor.execute( "INSERT INTO People (pk, username, full_name,compjson) \
+                    VALUES (pk, username,full_name,compjson) \
+                               ")
+    mariadb_connection.commit()
+    mariadb_connection.close()
+
+
+def linkMP(MediaID, pk):
+    with open("config.json","r") as fichier:
+        conf = json.load(fichier) 
+    
+    username = conf["DB"]["USER"]
+    pwd = conf["DB"]["PASSWORD"]
+    db = conf["DB"]["DATABASE"]
+
+    mariadb_connection = mariadb.connect(user= username, password=pwd, database=db)
+    cursor = mariadb_connection.cursor()
+    
+    cursor.execute( "INSERT INTO Media_likers () \
+                    VALUES () \
+                               ")
+    mariadb_connection.commit()
+    mariadb_connection.close()
