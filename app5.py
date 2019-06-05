16ee15561c4a90b0bf8f2195ce348c18ce9da659
MichaelDupont <michael@homedupont.fr>
2019-06-05 17:53:26 +0200

    ajout angular page accueil

--- app5.py
+++ app5.py
@@ -18,7 +18,7 @@ def hello():
         conf = json.load(fichier) 
     username = conf["INSTAGRAM"]["USER"]
     # 
-    return send_from_directory('./angular/', 'index.html')
+    return render_template("index.html", user=username)
 
 @MyApp.route('/user/<username>')
 def show_user_profile(username):
